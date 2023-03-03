7d2477140a81d5f36ebf59da226ecbec13c73aec
Abi Turner <turner.abi@gmail.com>
2023-03-03 21:27:25 +0000

    created tests for artist and updated artist controller function to pass all tests

--- src/routes/artist.js
+++ src/routes/artist.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const router = express.Router();
 const artistController = require('../controllers/artist');
 
-const router = express.Router();
 
 router.post('/', artistController.createArtist);
+
+router.get('/', artistController.getAllArtists);
+
+router.get('/:id', artistController.getArtistById);
+
+router.patch('/:id', artistController.updateArtist);
+
+router.delete('/:id', artistController.deleteArtistById);
 
 module.exports = router;
